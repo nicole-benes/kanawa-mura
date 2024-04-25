b3cd7ba4db31bd00c83c202bbe5bb327ae7eb67b
Nicole Benes <nicolembenes@gmail.com>
2024-04-25 02:32:31 -0400

    Adding tattoos

---
+++ haiku/tattoos.php
@@ -0,0 +1,56 @@
+<?php
+
+// Try to load my dump of the equipment page
+$handle = fopen("source/tattoos.wiki", "r");
+
+// Set up a placeholder for our 
+$tats = [];
+
+// Ensure we loaded the file
+if ( $handle ) {
+
+     // Ye Olde Scope Declarations
+     $tat = [];
+
+     // Read the next line in the file
+	while ( ( $line = fgets( $handle )) !== false ) {
+
+		// Check if this is a line denoting a category
+		if( strpos( $line, '* **' ) !== false ) { 
+
+               // Reset the tat array
+               $tat = [];
+
+               // Find where the title ends
+               $titleEnd = strpos( $line, ':**' );
+
+               // Pull out the name
+               $tat[ 'name' ] = trim( substr( $line, 4, $titleEnd - 4 ) );
+
+               // Pull out the description
+               $tat[ 'description' ] = trim( substr( $line, $titleEnd + 3 ) );
+
+               // Assume source is CR
+               $tat[ 'source' ] = 'CR';
+
+               // Assume Togashi Monks
+               $tat[ 'keywords' ] = [
+                    'Togashi Tattooed Order',
+                    'Monk',
+               ];
+
+               // Push this tat anto the array
+               $tats[] = $tat;
+
+          // Never should be another kind of line
+          } else {
+
+               // Output an error and quit
+               echo "Unparsed line: \n$line\n";
+               exit();
+          }
+     }
+
+	// Write our json file
+	file_put_contents( '../web/modules/custom/last_haiku_import/json/tattoos.json', json_encode( $tats ) );      
+}
